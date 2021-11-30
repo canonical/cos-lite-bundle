8c6f16ef655d047d9bc985677853b2878e5b99d5
sed-i <82407168+sed-i@users.noreply.github.com>
2021-11-30 12:13:57 -0500

    use period with microk8s usb commands

--- tests/load/gcp/lma_startup_script.tpl.sh
+++ tests/load/gcp/lma_startup_script.tpl.sh
@@ -20,9 +20,9 @@ sh -c "until microk8s.kubectl rollout status daemonset.apps/speaker -n metallb-s
 # workaround for
 # ERROR resolving microk8s credentials: max duration exceeded: secret for service account "juju-credential-microk8s" not found
 # Ref: https://github.com/charmed-kubernetes/actions-operator/blob/main/src/bootstrap/index.ts
-microk8s kubectl create serviceaccount test-sa
-timeout 600 sh -c "until microk8s kubectl get secrets | grep -q test-sa-token-; do sleep 5; done"
-microk8s kubectl delete serviceaccount test-sa
+microk8s.kubectl create serviceaccount test-sa
+timeout 600 sh -c "until.microk8s kubectl get secrets | grep -q test-sa-token-; do sleep 5; done"
+microk8s.kubectl delete serviceaccount test-sa
 
 sudo -u ubuntu juju bootstrap --no-gui microk8s uk8s
 sudo -u ubuntu juju add-model --config logging-config="<root>=WARNING; unit=DEBUG" --config update-status-hook-interval="60m" lma-load-test
