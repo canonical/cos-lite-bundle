68278763baa2ea48c5eb0d8d0dda455af0333753
sed-i <82407168+sed-i@users.noreply.github.com>
2021-12-01 00:43:47 -0500

    split load-test.tf into .tf per vm instance

---
+++ tests/load/gcp/prom-scrape.tf
@@ -0,0 +1,34 @@
+data "cloudinit_config" "avalanche" {
+  gzip = false
+  base64_encode = false
+
+  part {
+    content_type = "text/cloud-config"
+    content = templatefile("prom-scrape-avalanche.tpl.conf", { METRIC_COUNT = var.avalanche_metric_count, VALUE_INTERVAL = var.avalanche_value_interval })
+    filename = "avalanche.conf"
+  }
+}
+
+resource "google_compute_instance" "vm_avalanche" {
+  name         = "avalanche"
+  machine_type = "custom-4-16384"
+  tags         = ["load-test-traffic"]
+
+  boot_disk {
+    initialize_params {
+      image = "projects/lma-light-load-testing/global/images/avalanche"
+    }
+  }
+
+  metadata = {
+    user-data = "${data.cloudinit_config.avalanche.rendered}"
+  }
+
+  network_interface {
+    network = google_compute_network.net_lma_light_load_test_net.name
+
+    access_config {
+    }
+  }
+}
+
