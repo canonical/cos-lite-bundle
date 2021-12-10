c49cc378917a79d06e08d73cafb00cbb1565d1f6
sed-i <82407168+sed-i@users.noreply.github.com>
2021-12-10 18:39:34 -0500

    reduce big query to 11k points (~15 MB)

--- tests/load/gcp/prom-query-locustfile.tpl.py
+++ tests/load/gcp/prom-query-locustfile.tpl.py
@@ -19,7 +19,7 @@ def normalize(*args):
 
 def probabilities_to_weights(*args):
     """Scale a sequence of probabilities into integer weights.
-    
+
     Locust's `@task` decorator takes integer weight rather than probability.
     """
     # Rescale args such that the lowest arg becomes 1, then multiply by
@@ -32,8 +32,8 @@ def probabilities_to_weights(*args):
 
 GRAFANA_DASHBOARD_REFRESH_PERIOD = ${REFRESH_INTERVAL}  # seconds
 
-# simulate a "full timeseries" fetch (large query) every 10 minutes (per panel)
-entire_panel_fetch_period = 10 * 60  # 10 minutes
+# simulate a "full timeseries" fetch (large query) every 5 minutes (per panel)
+entire_panel_fetch_period = 5 * 60  # 5 minutes
 incremental_fetch_period = GRAFANA_DASHBOARD_REFRESH_PERIOD
 
 entire_panel_fetch_weight, incremental_fetch_weight = probabilities_to_weights(
@@ -51,30 +51,23 @@ class PromTest1(FastHttpUser):
     @task(weight=incremental_fetch_weight)
     def query_incremental(self):
         """Only query point received during the past scrape interval (that's how grafana updates panels)."""
-        
-        # Fetching the double of GRAFANA_DASHBOARD_REFRESH_PERIOD to account for potentially missed 
+
+        # Fetching the double of GRAFANA_DASHBOARD_REFRESH_PERIOD to account for potentially missed
         # data points missed around interval boundaries or lost packets
         # A factor of 2 might be an overkill but is still small enough to not care much about it.
+        metric_num = random.randint(0, ASSUMED_NUM_WORKERS - 1)
+        incremental_period = 2 * GRAFANA_DASHBOARD_REFRESH_PERIOD
         self.client.get(
-            "/api/v1/query?query=avalanche_metric_mmmmm_0_{}".format(random.randint(0, ASSUMED_NUM_WORKERS - 1)) + \
-            "{series_id='0'}" + "[{}s]".format(2 * GRAFANA_DASHBOARD_REFRESH_PERIOD)
+            "/api/v1/query?query=avalanche_metric_mmmmm_0_{}".format(metric_num) + \
+            "{series_id='0'}" + "[{}s]".format(incremental_period)
         )
 
     @task(weight=entire_panel_fetch_weight)
     def query_panel_startup(self):
         """"Get lots of data, representing a query for a newly loaded panel."""
-        # ~10MB
-        # /api/v1/query?query=rate(avalanche_metric_mmmmm_0_0[5m])[60m:130ms]
-        #
-        # ~100MB
-        # /api/v1/query?query=rate(avalanche_metric_mmmmm_0_0[5m])[60m:13ms]
-        upper = 130
-        lower = 13
-        mid = int(lower + (upper - lower) / 2)
-        seq = [lower, mid, upper]
+        # Sensible timeseries resolution is 11k points
+        # "This is sufficient for 60s resolution for a week or 1h resolution for a year."
+        # Ref: https://www.robustperception.io/limiting-promql-resource-usage
 
-        self.client.get(
-            "/api/v1/query?query=rate(avalanche_metric_mmmmm_0_0[5m])"
-            f"[60m:{random.choice(seq)}ms]"
-        )
+        self.client.get("/api/v1/query?query=rate(avalanche_metric_mmmmm_0_0[5m])[3300s:300ms]")
 
