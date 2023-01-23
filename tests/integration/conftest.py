2ca7818ce81a667e32497675ee6b2cdc5fb98657
sed-i <82407168+sed-i@users.noreply.github.com>
2023-01-23 01:27:50 -0500

    Cleanup leftover

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,10 +1,6 @@
 # Copyright 2021 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-import logging
-
-logger = logging.getLogger(__name__)
-
 
 def pytest_addoption(parser):
     # not providing the "default" arg to addoption: the bundle template already specifies defaults
