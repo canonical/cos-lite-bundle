c0026037690d209a81b4a6534350cc0ceb21065b
sed-i <82407168+sed-i@users.noreply.github.com>
2024-03-27 23:40:39 -0400

    Do not render bundle with pytest

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -18,47 +18,12 @@ def get_this_script_dir() -> Path:
     return Path(path)
 
 
-def pytest_addoption(parser):
-    # not providing the "default" arg to addoption: the bundle template already specifies defaults
-    parser.addoption("--traefik", action="store")
-    parser.addoption("--alertmanager", action="store")
-    parser.addoption("--prometheus", action="store")
-    parser.addoption("--grafana", action="store")
-    parser.addoption("--loki", action="store")
-    parser.addoption("--avalanche", action="store")
-    parser.addoption("--channel", action="store")
-
-
 @pytest.fixture(scope="module")
-async def rendered_bundle(ops_test: OpsTest, pytestconfig) -> Path:
+async def rendered_bundle() -> Path:
     """Returns the pathlib.Path for the rendered bundle file."""
-    logger.info("Rendering bundle %s", get_this_script_dir() / ".." / ".." / "bundle.yaml.j2")
+    # If a bundle.yaml file already exists, use it (do no render anything).
+    user_bundle = get_this_script_dir() / ".." / ".." / "bundle.yaml"
+    if not user_bundle.exists():
+        raise FileNotFoundError("Expected a 'bundle.yaml' to be present")
 
-    async def build_charm_if_is_dir(option: str) -> str:
-        if Path(option).is_dir():
-            logger.info("Building charm from source: %s", option)
-            option = str(await ops_test.build_charm(option))
-        return option
-
-    charms = {
-        "traefik": pytestconfig.getoption("traefik"),
-        "alertmanager": pytestconfig.getoption("alertmanager"),
-        "prometheus": pytestconfig.getoption("prometheus"),
-        "grafana": pytestconfig.getoption("grafana"),
-        "loki": pytestconfig.getoption("loki"),
-        "avalanche": pytestconfig.getoption("avalanche"),
-    }
-
-    additional_args = {
-        "channel": pytestconfig.getoption("channel"),
-    }
-
-    context = {k: await build_charm_if_is_dir(v) for k, v in charms.items() if v is not None}
-    context.update(additional_args)
-    logger.debug("context: %s", context)
-
-    rendered_bundle = ops_test.render_bundle(
-        get_this_script_dir() / ".." / ".." / "bundle.yaml.j2", context=context
-    )
-
-    return rendered_bundle
+    return user_bundle
