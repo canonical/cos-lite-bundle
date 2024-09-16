9b37cc528abfb376847bbee8b399d99d15543c70
Michael Thamm <mike.thamm@canonical.com>
2024-09-16 14:29:04 -0400

    feat: more goss.yaml imports and scripting

--- goss/scripts/compare-dashboard-uids.sh
+++ goss/scripts/compare-dashboard-uids.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Collect the UID from the charm dashboard and make sure it is a subset of the UIDs in Grafana
+# Collect the UID from the charm dashboard and check if it is a subset of Grafana dashboard UIDs
 
 MODEL_NAME="$1"
 GRAFANA_APP_NAME="$2"
@@ -9,15 +9,15 @@ GRAFANA_DASHBOARD_DIR="/etc/${GRAFANA_APP_NAME}/provisioning/dashboards/"
 OTHER_APP_DASHBOARD_DIR="agents/unit-${OTHER_APP_NAME}-0/charm/src/grafana_dashboards/"
 TARGET_UID=$(juju ssh ${OTHER_APP_NAME}/0 "cat ${OTHER_APP_DASHBOARD_DIR}/${OTHER_APP_NAME}*.json.tmpl" | jq -r '.uid')
 
-# Get the list of JSON files in the Grafana dashboard directory inside the container
-file_list=$(juju ssh -m ${MODEL_NAME} --container ${GRAFANA_APP_NAME} ${GRAFANA_APP_NAME}/0 "ls ${GRAFANA_DASHBOARD_DIR}*.json")
+# Get the list of JSON files in the Grafana dashboard directory inside the workload container
+file_list=$(juju ssh -m ${MODEL_NAME} --container grafana ${GRAFANA_APP_NAME}/0 "ls ${GRAFANA_DASHBOARD_DIR}*.json")
 match_found=false
 
 # Iterate over each dashboard file
 for file_path in ${file_list}; do
 
   # Get the contents of each dashboard and use jq to extract the 'uid'
-  uid=$(juju ssh -m ${MODEL_NAME} --container ${GRAFANA_APP_NAME} ${GRAFANA_APP_NAME}/0 "cat ${file_path}" | jq -r '.uid')
+  uid=$(juju ssh -m ${MODEL_NAME} --container grafana ${GRAFANA_APP_NAME}/0 "cat ${file_path}" | jq -r '.uid')
 
   # Check if UID extraction was successful
   if [ -n "$uid" ]; then
@@ -29,7 +29,7 @@ for file_path in ${file_list}; do
 done
 
 if [ "$match_found" = true ]; then
-  echo "The ${OTHER_APP_NAME} dashboard UID is a subset of the Grafana dashboard UIDs."
+  echo "Match found."
 else
-  echo "No matches found."
+  echo "The ${OTHER_APP_NAME} dashboard UID is not a subset of the Grafana dashboard UIDs."
 fi
